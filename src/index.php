a481cdea2fda5840ea2acf73fbdebfcc053cc8d9
Brad Ball <brad.ball@cdinteractive.com>
2017-01-03 21:56:56 -0500

    more homepage hours updates

--- src/index.php
+++ src/index.php
@@ -38,34 +38,23 @@
 
 					<div class="row site-links">
 
-						<div class="col-md-12 col-lg-6 col-lg-push-6">
-							<div class="hours">
-								<?php
-									$now = time();
-									$winter_start = mktime(0,0,0,12,1,2016);
-									$winter_end = mktime(0,0,0,3,31,2017)
-								?>
+						<?php
+							$now = time();
+							$winter_start = mktime(0,0,0,12,1,2016);
+							$winter_end = mktime(0,0,0,3,31,2017)
+						?>
 
+						<div class="col-md-12 col-lg-6">
+							<img src="/_/img/home/museum-flag.jpg" class="img-fluid" />
+
+							<div id="Announcement" class="hours">
 								<?php if ($now > $winter_start && $now < $winter_end) : ?>
-									<h6>WINTER MUSEUM HOURS</h6>
 									<p>
+										<b class="date-header">Winter Museum Hours</b><br />
 										January 1 - March 31, 2017<br />
 								        Tue-Sat: 10:00am - 3:00pm
 								    </p>								    
-								<?php else: ?>
-									<h6>MUSEUM HOURS</h6>
-								    <p>
-										April 1 - December 31, 2017<br />
-									    Tue-Sat: 10:00am - 4:30pm
-									</p>
 							    <?php endif; ?>
-							    <p>Closed Saturdays &amp; Sundays</p>
-								<p class="admission">
-									Free Admission
-								</p>								
-							</div>
-							<div class="panel">
-
 							    <p>
 							    	<b class="date-header">Regular Museum Hours</b><br />
 									April 1 - December 31, 2017<br />
@@ -75,17 +64,20 @@
 								<p><b class="date-header">Open:</b><br />
 									May 29, 2017 Memorial Day <br />
 									July 4, 2017 Independence Day        <br />
-									November 10, 2017 Veterans Day (observed)<br />
+									November 10, 2017 Veterans Day<br />
 									November 11, 2017 Veterans Day
 								</p>
 
+							</div>
+
+						</div>
+
+						<div class="col-md-12 col-lg-6">
+							<div class="hours">
+
 								<p><b class="date-header">Closed:</b></br>     
 									Sundays &amp; Mondays<br />
-
-									<?php if (date("m") == "12") :?><span class="date-callout date-header" style="padding: .25em .15em"><?php endif; ?>
-							        December 19 - December 31, 2016
-									<?php if (date("m") == "12") :?></span><?php endif; ?>
-									 <br />
+									<br />
 							        January 1, 2017 New Year's Day<br />
 							        January 2, 2017 New Year's Day (observed)<br />
 							        January 16, 2017 Martin Luther King Day<br />
@@ -95,34 +87,9 @@
 							        November 23, 2017 Thanksgiving Day<br />
 							        December 25, 2017 Christmas Day
 						        </p>
-
-
-
 							</div>
 						</div>
 
-						<div class="col-md-12 col-lg-6 col-lg-pull-6">
-							<img src="/_/img/home/museum-flag.jpg" class="img-fluid" />
-
-							<div id="Announcement">
-								<h3>UPCOMING EVENTS</h3>
-								<?php if (time() < mktime(0,0,0,11,11,2016)): ?>
-									<p>
-			                            <b><a href="/_/res/pdf/coin-giveaway.pdf">GEORGE S.  PATTON  COIN GIVEAWAY</a></b><br />
-                                        November 7th - 10th
-                                    </p>
-                                <?php endif; ?>
-
-								<?php if (time() < mktime(0,0,0,11,13,2016)): ?>
-									<p>
-										<b><a href="/_/res/pdf/art-of-war.pdf">MILITARY MODEL AND ART SHOW</a></b><br />
-        	                            November 12th, 2016<br />                                    
-            	                        10:00 AM - 4.30 PM
-                                     </p>
-                                <?php endif; ?>
-							</div>
-
-						</div>
 
 					</div>
 
